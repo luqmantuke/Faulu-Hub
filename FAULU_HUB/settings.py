50d16eea667396231cf37431d119940f53e7cd59
smartcodes-tz <lsuleiman2002@gmail.com>
2020-12-17 13:57:05 +0300

    fixing server error

--- FAULU_HUB/settings.py
+++ FAULU_HUB/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9(9f9d^dxp$_*@zw=cj%2bunoi@#%04i4w39+st($we6!r=ay2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['fauluhub.com', 'faulu-hub.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
