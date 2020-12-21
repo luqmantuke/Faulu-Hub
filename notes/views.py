c99eca2db84da73ccc3d40b0c98416253a896e69
smartcodes-tz <lsuleiman2002@gmail.com>
2020-12-21 05:03:08 +0300

    adding search functionality on notes

--- notes/views.py
+++ notes/views.py
@@ -4,6 +4,7 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
 from .models import *
 from .filters import NotesFilter
+from django.db.models import Q
 
 
 def content_list(request):
@@ -14,6 +15,7 @@ def content_list(request):
 
     return render(request, template_name, {'content': notes,
         'myfilter': myFilter})
+
 
 def content_detail(request, slug):
     q = Content.objects.filter(slug__iexact = slug)
@@ -27,3 +29,15 @@ def content_detail(request, slug):
     }
 
     return render(request, 'notes/content_detail.html', context)
+
+
+class SearchResultsView(ListView):
+    model = Content
+    template_name = 'notes/search_results.html'
+
+    def get_queryset(self): # new
+        query = self.request.GET.get('q')
+        object_list = Content.objects.filter(
+            Q(name__icontains=query) | Q(body__icontains=query)
+        )
+        return object_list
