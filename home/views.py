5538f9a4373b45bdcb4a8930d6f3205714fbdf74
smartcodes-tz <lsuleiman2002@gmail.com>
2020-12-31 15:51:34 +0300

    adding terms and privacy

--- home/views.py
+++ home/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
-from blog.models import Post
 
 
 def index(request):
-    posts = Post.objects.filter(status='published').order_by('-created')[0:6]
-    context = {
-        'post': posts
-    }
 
-    return render(request, 'index-4.html', context)
+    return render(request, 'index-4.html')
 
+
+def privacy(request):
+    return render(request, 'privacy.html',)
